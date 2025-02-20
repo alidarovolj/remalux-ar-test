992a8b329587da1517af503e39b1e688188a9f03
alidarovolj <alidarovolzhas@gmail.com>
2025-02-21 00:11:19 +0500

    fixes

--- Assets/Scripts/Managers/WallDetectionManager.cs
+++ Assets/Scripts/Managers/WallDetectionManager.cs
@@ -8,20 +8,22 @@ public class WallDetectionManager : MonoBehaviour
 {
     [SerializeField] private ARPlaneManager planeManager;
     [SerializeField] private Material wallMaterial;
-    [SerializeField] private float minPlaneArea = 0.09f;
-    [SerializeField] private float maxWallGap = 1.0f;
-    [SerializeField] private float angleThreshold = 20f;
+    [SerializeField] private float minPlaneArea = 0.25f;
+    [SerializeField] private float maxWallGap = 0.15f;
+    [SerializeField] private float planeMergeAngleThreshold = 8f;
+    [SerializeField] private float updateInterval = 0.5f;
+    [SerializeField] private float edgeDistanceThreshold = 0.2f;
+    [SerializeField] private float heightMatchThreshold = 0.3f;
 
     private Dictionary<TrackableId, WallInfo> detectedWalls = new Dictionary<TrackableId, WallInfo>();
+    private float lastUpdateTime;
 
     private void Start()
     {
-        Debug.Log("WallDetectionManager: Start");
-        
         if (planeManager != null)
         {
             planeManager.requestedDetectionMode = PlaneDetectionMode.Vertical;
-            Debug.Log($"WallDetectionManager: Plane Manager найден, режим = {planeManager.requestedDetectionMode}");
+            EnablePlaneDetection();
         }
         else
         {
@@ -34,35 +36,113 @@ public class WallDetectionManager : MonoBehaviour
         }
     }
 
-    private void Update()
+    private void OnEnable()
+    {
+        EnablePlaneDetection();
+    }
+
+    private void OnDisable()
+    {
+        DisablePlaneDetection();
+        detectedWalls.Clear();
+    }
+
+    private void EnablePlaneDetection()
     {
         if (planeManager != null)
         {
-            // Создаем временный список для удаления недействительных стен
-            var invalidWalls = new List<TrackableId>();
-            
-            // Проверяем существующие стены
-            foreach (var wallPair in detectedWalls)
-            {
-                if (!planeManager.trackables.TryGetTrackable(wallPair.Key, out ARPlane _))
-                {
-                    invalidWalls.Add(wallPair.Key);
-                }
-            }
-            
-            // Удаляем недействительные стены
-            foreach (var id in invalidWalls)
-            {
-                detectedWalls.Remove(id);
-            }
+            planeManager.enabled = true;
+            planeManager.planesChanged += HandlePlanesChanged;
+        }
+    }
 
-            // Обрабатываем текущие плоскости
-            foreach (var plane in planeManager.trackables)
+    private void DisablePlaneDetection()
+    {
+        if (planeManager != null)
+        {
+            planeManager.planesChanged -= HandlePlanesChanged;
+            planeManager.enabled = false;
+        }
+    }
+
+    private void HandlePlanesChanged(ARPlanesChangedEventArgs eventArgs)
+    {
+        // Обработка добавленных плоскостей
+        foreach (ARPlane plane in eventArgs.added)
+        {
+            if (plane != null)
             {
-                if (plane != null && plane.gameObject != null)
+                plane.boundaryChanged += OnPlaneBoundaryChanged;
+            }
+        }
+
+        // Обработка удаленных плоскостей
+        foreach (ARPlane plane in eventArgs.removed)
+        {
+            if (plane != null)
+            {
+                plane.boundaryChanged -= OnPlaneBoundaryChanged;
+                if (detectedWalls.ContainsKey(plane.trackableId))
                 {
-                    ProcessPlane(plane);
+                    detectedWalls.Remove(plane.trackableId);
                 }
+            }
+        }
+
+        // Обработка обновленных плоскостей
+        foreach (ARPlane plane in eventArgs.updated)
+        {
+            if (plane != null && plane.gameObject != null)
+            {
+                ProcessPlane(plane);
+            }
+        }
+    }
+
+    private void OnPlaneBoundaryChanged(ARPlaneBoundaryChangedEventArgs args)
+    {
+        if (Time.time - lastUpdateTime < updateInterval) return;
+        lastUpdateTime = Time.time;
+
+        var plane = args.plane;
+        ProcessPlane(plane);
+        MergeWalls();
+    }
+
+    private void Update()
+    {
+        if (Time.time - lastUpdateTime < updateInterval) return;
+        lastUpdateTime = Time.time;
+
+        if (planeManager != null)
+        {
+            UpdateWalls();
+            MergeWalls();
+        }
+    }
+
+    private void UpdateWalls()
+    {
+        var invalidWalls = new List<TrackableId>();
+
+        foreach (var wallPair in detectedWalls)
+        {
+            if (!planeManager.trackables.TryGetTrackable(wallPair.Key, out ARPlane _))
+            {
+                invalidWalls.Add(wallPair.Key);
+            }
+        }
+
+        foreach (var id in invalidWalls)
+        {
+            detectedWalls.Remove(id);
+        }
+
+        foreach (var plane in planeManager.trackables)
+        {
+            if (plane != null && plane.gameObject != null)
+            {
+                ProcessPlane(plane);
             }
         }
     }
@@ -77,44 +157,138 @@ public class WallDetectionManager : MonoBehaviour
 
         if (area < minPlaneArea)
         {
-            Debug.Log($"WallDetectionManager: Плоскость {plane.trackableId} слишком мала: {size}");
             return;
         }
 
-        // Проверяем соседние стены
-        foreach (var wallPair in detectedWalls)
-        {
-            if (!planeManager.trackables.TryGetTrackable(wallPair.Key, out ARPlane existingPlane))
-                continue;
-
-            float distance = Vector3.Distance(plane.transform.position, existingPlane.transform.position);
-            float angle = Vector3.Angle(plane.transform.up, existingPlane.transform.up);
-
-            if (distance < maxWallGap && Mathf.Abs(angle - 90f) < angleThreshold)
-            {
-                float existingArea = existingPlane.size.x * existingPlane.size.y;
-                if (area < existingArea)
-                {
-                    Debug.Log($"WallDetectionManager: Плоскость {plane.trackableId} пропущена как часть угла");
-                    return;
-                }
-            }
-        }
-
-        // Обновляем или добавляем информацию о стене
         if (!detectedWalls.ContainsKey(plane.trackableId))
         {
             detectedWalls[plane.trackableId] = new WallInfo(plane, planeManager);
-            Debug.Log($"WallDetectionManager: Добавлена новая стена {plane.trackableId}");
-        }
 
-        var meshRenderer = plane.GetComponent<MeshRenderer>();
-        if (meshRenderer != null && wallMaterial != null)
-        {
-            meshRenderer.material = wallMaterial;
+            var meshRenderer = plane.GetComponent<MeshRenderer>();
+            if (meshRenderer != null && wallMaterial != null)
+            {
+                meshRenderer.material = wallMaterial;
+            }
         }
 
         detectedWalls[plane.trackableId].UpdateBoundaries();
+    }
+
+    private void MergeWalls()
+    {
+        bool mergedAny;
+        do
+        {
+            mergedAny = false;
+            var wallsToMerge = new List<(TrackableId, TrackableId)>();
+
+            foreach (var wall1 in detectedWalls)
+            {
+                if (!wall1.Value.isActive) continue;
+
+                foreach (var wall2 in detectedWalls)
+                {
+                    if (!wall2.Value.isActive || wall1.Key == wall2.Key) continue;
+
+                    if (planeManager.trackables.TryGetTrackable(wall1.Key, out ARPlane plane1) &&
+                        planeManager.trackables.TryGetTrackable(wall2.Key, out ARPlane plane2))
+                    {
+                        if (ShouldMergePlanes(plane1, plane2))
+                        {
+                            wallsToMerge.Add((wall1.Key, wall2.Key));
+                            mergedAny = true;
+                            break;
+                        }
+                    }
+                }
+                if (mergedAny) break;
+            }
+
+            foreach (var (wall1Id, wall2Id) in wallsToMerge)
+            {
+                MergeWallPair(wall1Id, wall2Id);
+            }
+        } while (mergedAny);
+    }
+
+    private bool ShouldMergePlanes(ARPlane plane1, ARPlane plane2)
+    {
+        // Проверяем расстояние между центрами
+        float distance = Vector3.Distance(plane1.center, plane2.center);
+        if (distance > maxWallGap) return false;
+
+        // Проверяем угол между нормалями
+        float angle = Vector3.Angle(plane1.normal, plane2.normal);
+        if (angle > planeMergeAngleThreshold) return false;
+
+        // Проверяем высоту
+        float heightDiff = Mathf.Abs(plane1.center.y - plane2.center.y);
+        if (heightDiff > heightMatchThreshold) return false;
+
+        // Проверяем наличие общего края
+        return HasSharedEdge(plane1, plane2);
+    }
+
+    private bool HasSharedEdge(ARPlane plane1, ARPlane plane2)
+    {
+        var bounds1 = detectedWalls[plane1.trackableId].boundaries;
+        var bounds2 = detectedWalls[plane2.trackableId].boundaries;
+
+        for (int i = 0; i < bounds1.Length; i++)
+        {
+            Vector3 edge1Start = bounds1[i];
+            Vector3 edge1End = bounds1[(i + 1) % bounds1.Length];
+
+            for (int j = 0; j < bounds2.Length; j++)
+            {
+                Vector3 edge2Start = bounds2[j];
+                Vector3 edge2End = bounds2[(j + 1) % bounds2.Length];
+
+                if (EdgesAreClose(edge1Start, edge1End, edge2Start, edge2End))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private bool EdgesAreClose(Vector3 edge1Start, Vector3 edge1End, Vector3 edge2Start, Vector3 edge2End)
+    {
+        float distStart = Mathf.Min(
+            Vector3.Distance(edge1Start, edge2Start),
+            Vector3.Distance(edge1Start, edge2End)
+        );
+        float distEnd = Mathf.Min(
+            Vector3.Distance(edge1End, edge2Start),
+            Vector3.Distance(edge1End, edge2End)
+        );
+        return distStart < edgeDistanceThreshold && distEnd < edgeDistanceThreshold;
+    }
+
+    private void MergeWallPair(TrackableId wall1Id, TrackableId wall2Id)
+    {
+        if (!detectedWalls.ContainsKey(wall1Id) || !detectedWalls.ContainsKey(wall2Id))
+            return;
+
+        var wall1 = detectedWalls[wall1Id];
+        var wall2 = detectedWalls[wall2Id];
+
+        if (planeManager.trackables.TryGetTrackable(wall1Id, out ARPlane plane1) &&
+            planeManager.trackables.TryGetTrackable(wall2Id, out ARPlane plane2))
+        {
+            // Объединяем границы
+            wall1.MergeWith(wall2);
+
+            // Обновляем меш и материал
+            var meshFilter1 = plane1.GetComponent<MeshFilter>();
+            if (meshFilter1 != null && meshFilter1.mesh != null)
+            {
+                // Деактивируем вторую стену
+                wall2.isActive = false;
+                plane2.gameObject.SetActive(false);
+            }
+        }
     }
 
     public class WallInfo
@@ -123,17 +297,22 @@ public class WallDetectionManager : MonoBehaviour
         public Vector3[] boundaries;
         public HashSet<TrackableId> connectedWalls;
         private ARPlaneManager planeManager;
+        public List<Vector3> mergedBoundaries;
+        public bool isActive = true;
 
         public WallInfo(ARPlane plane, ARPlaneManager manager)
         {
             this.trackableId = plane.trackableId;
             this.planeManager = manager;
             this.connectedWalls = new HashSet<TrackableId>();
+            this.mergedBoundaries = new List<Vector3>();
             UpdateBoundaries();
         }
 
         public void UpdateBoundaries()
         {
+            if (!isActive) return;
+
             if (planeManager != null && planeManager.trackables.TryGetTrackable(trackableId, out ARPlane plane))
             {
                 var mesh = plane.GetComponent<MeshFilter>()?.mesh;
@@ -144,13 +323,29 @@ public class WallDetectionManager : MonoBehaviour
                     {
                         boundaries[i] = plane.transform.TransformPoint(mesh.vertices[i]);
                     }
+                    if (mergedBoundaries.Count == 0)
+                    {
+                        mergedBoundaries = boundaries.ToList();
+                    }
                 }
             }
         }
-    }
 
-    private void OnDisable()
-    {
-        detectedWalls.Clear();
+        public void MergeWith(WallInfo other)
+        {
+            if (!isActive || !other.isActive) return;
+
+            // Добавляем уникальные точки границ
+            foreach (var point in other.mergedBoundaries)
+            {
+                if (!mergedBoundaries.Any(p => Vector3.Distance(p, point) < 0.01f))
+                {
+                    mergedBoundaries.Add(point);
+                }
+            }
+
+            connectedWalls.UnionWith(other.connectedWalls);
+            connectedWalls.Add(other.trackableId);
+        }
     }
 }
--- Assets/Scripts/Managers/WallPaintingManager.cs
+++ Assets/Scripts/Managers/WallPaintingManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using System.Collections.Generic;
+using System.Linq;
 
 public class WallPaintingManager : MonoBehaviour
 {
@@ -10,8 +11,14 @@ public class WallPaintingManager : MonoBehaviour
     [SerializeField] private ARPlaneManager planeManager;
     [SerializeField] private Material wallMaterial;
     [SerializeField] private Material highlightMaterial;
+    [SerializeField] private float previewAlpha = 0.5f; // Прозрачность предпросмотра
+    [SerializeField] private float colorTransitionSpeed = 5f; // Скорость перехода цвета
 
-    private Dictionary<ARPlane, GameObject> paintedWalls = new Dictionary<ARPlane, GameObject>();
+    private Dictionary<TrackableId, PaintedWallInfo> paintedWalls = new Dictionary<TrackableId, PaintedWallInfo>();
+    private ARPlane currentHighlightedPlane;
+    private GameObject previewObject;
+    private Stack<PaintAction> undoStack = new Stack<PaintAction>();
+    private const int MAX_UNDO_STEPS = 20;
 
     void Awake()
     {
@@ -26,121 +33,240 @@ public class WallPaintingManager : MonoBehaviour
     void Start()
     {
         Debug.Log("WallPaintingManager: Start вызван");
-        
+
         // Проверка компонентов
         Debug.Log($"ColorManager: {(colorManager != null ? "найден" : "не найден")}");
         Debug.Log($"RaycastManager: {(raycastManager != null ? "найден" : "не найден")}");
         Debug.Log($"PlaneManager: {(planeManager != null ? "найден" : "не найден")}");
         Debug.Log($"WallMaterial: {(wallMaterial != null ? "найден" : "не найден")}");
         Debug.Log($"HighlightMaterial: {(highlightMaterial != null ? "найден" : "не найден")}");
+
+        if (colorManager == null || raycastManager == null || planeManager == null ||
+            wallMaterial == null || highlightMaterial == null)
+        {
+            Debug.LogError("WallPaintingManager: Отсутствуют необходимые компоненты!");
+            enabled = false;
+            return;
+        }
+
+        // Создаем объект предпросмотра
+        previewObject = new GameObject("PaintPreview");
+        previewObject.AddComponent<MeshFilter>();
+        var previewRenderer = previewObject.AddComponent<MeshRenderer>();
+        previewRenderer.material = new Material(highlightMaterial);
+        previewRenderer.material.color = new Color(1f, 1f, 1f, previewAlpha);
+        previewObject.SetActive(false);
     }
 
     void Update()
     {
-        // Проверка через GetMouseButton
-        if (Input.GetMouseButtonDown(0))
-        {
-            Debug.Log("WallPaintingManager: Клик мыши обнаружен");
-            HandleTouch(Input.mousePosition);
-        }
+        UpdatePreview();
+        UpdatePaintedWalls();
+        HandleInput();
+    }
 
-        // Проверка через Input.touches
-        if (Input.touchCount > 0)
+    private void UpdatePreview()
+    {
+        ARPlane hitPlane = GetHitPlane();
+
+        if (hitPlane != null && hitPlane.alignment == PlaneAlignment.Vertical)
         {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
+            if (currentHighlightedPlane != hitPlane)
             {
-                Debug.Log($"WallPaintingManager: Касание обнаружено в позиции {touch.position}");
-                HandleTouch(touch.position);
+                currentHighlightedPlane = hitPlane;
+                UpdatePreviewMesh(hitPlane);
+            }
+            previewObject.SetActive(true);
+        }
+        else
+        {
+            currentHighlightedPlane = null;
+            previewObject.SetActive(false);
+        }
+    }
+
+    private void UpdatePreviewMesh(ARPlane plane)
+    {
+        var planeMesh = plane.GetComponent<MeshFilter>()?.mesh;
+        if (planeMesh == null) return;
+
+        var previewMeshFilter = previewObject.GetComponent<MeshFilter>();
+        previewMeshFilter.mesh = planeMesh;
+
+        previewObject.transform.position = plane.transform.position + plane.transform.forward * 0.001f;
+        previewObject.transform.rotation = plane.transform.rotation;
+        previewObject.transform.localScale = new Vector3(1.01f, 1.01f, 1f);
+
+        var previewRenderer = previewObject.GetComponent<MeshRenderer>();
+        previewRenderer.material.color = new Color(
+            colorManager.GetCurrentColor().r,
+            colorManager.GetCurrentColor().g,
+            colorManager.GetCurrentColor().b,
+            previewAlpha
+        );
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            if (currentHighlightedPlane != null)
+            {
+                PaintWall(currentHighlightedPlane);
             }
         }
 
-        // Обновляем позиции покрашенных стен
-        foreach (var kvp in new Dictionary<ARPlane, GameObject>(paintedWalls))
+        // Отмена последнего действия
+        if (Input.GetKeyDown(KeyCode.Z) && (Input.GetKey(KeyCode.LeftCommand) || Input.GetKey(KeyCode.RightCommand)))
         {
-            if (kvp.Key != null && kvp.Value != null)
-            {
-                kvp.Value.transform.position = kvp.Key.transform.position + kvp.Key.transform.forward * 0.0005f;
-                kvp.Value.transform.rotation = kvp.Key.transform.rotation;
-                kvp.Value.transform.localScale = new Vector3(1.02f, 1.02f, 1.0f);
+            UndoLastAction();
+        }
+    }
 
-                var planeMeshFilter = kvp.Key.GetComponent<MeshFilter>();
-                var wallMeshFilter = kvp.Value.GetComponent<MeshFilter>();
-                if (planeMeshFilter != null && wallMeshFilter != null && planeMeshFilter.mesh != null)
+    private void PaintWall(ARPlane plane)
+    {
+        Color newColor = colorManager.GetCurrentColor();
+
+        if (!paintedWalls.TryGetValue(plane.trackableId, out var wallInfo))
+        {
+            GameObject paintedWall = CreatePaintedWall(plane);
+            wallInfo = new PaintedWallInfo(paintedWall, newColor);
+            paintedWalls.Add(plane.trackableId, wallInfo);
+        }
+
+        // Сохраняем действие для отмены
+        undoStack.Push(new PaintAction(plane.trackableId, wallInfo.currentColor, newColor));
+        if (undoStack.Count > MAX_UNDO_STEPS)
+        {
+            undoStack = new Stack<PaintAction>(undoStack.Take(MAX_UNDO_STEPS));
+        }
+
+        wallInfo.targetColor = newColor;
+    }
+
+    private GameObject CreatePaintedWall(ARPlane plane)
+    {
+        GameObject wall = new GameObject($"PaintedWall_{plane.trackableId}");
+
+        wall.transform.position = plane.transform.position + plane.transform.forward * 0.001f;
+        wall.transform.rotation = plane.transform.rotation;
+        wall.transform.localScale = new Vector3(1.01f, 1.01f, 1f);
+
+        var meshFilter = wall.AddComponent<MeshFilter>();
+        var meshRenderer = wall.AddComponent<MeshRenderer>();
+
+        var planeMesh = plane.GetComponent<MeshFilter>()?.mesh;
+        if (planeMesh != null)
+        {
+            meshFilter.mesh = planeMesh;
+        }
+
+        meshRenderer.material = new Material(wallMaterial);
+        meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        meshRenderer.receiveShadows = false;
+
+        return wall;
+    }
+
+    private void UpdatePaintedWalls()
+    {
+        foreach (var wallInfo in paintedWalls.Values)
+        {
+            if (wallInfo.gameObject != null)
+            {
+                // Плавный переход цвета
+                wallInfo.currentColor = Color.Lerp(
+                    wallInfo.currentColor,
+                    wallInfo.targetColor,
+                    Time.deltaTime * colorTransitionSpeed
+                );
+
+                var renderer = wallInfo.gameObject.GetComponent<MeshRenderer>();
+                if (renderer != null)
                 {
-                    wallMeshFilter.mesh = planeMeshFilter.mesh;
+                    renderer.material.color = wallInfo.currentColor;
                 }
             }
         }
     }
 
-    private void HandleTouch(Vector2 touchPosition)
+    private ARPlane GetHitPlane()
     {
+        Vector2 screenPosition = Input.mousePosition;
+        if (Input.touchCount > 0)
+        {
+            screenPosition = Input.GetTouch(0).position;
+        }
+
         List<ARRaycastHit> hits = new List<ARRaycastHit>();
-        Debug.Log($"WallPaintingManager: Пытаемся сделать raycast из позиции {touchPosition}");
-        
-        if (raycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        if (raycastManager.Raycast(screenPosition, hits, TrackableType.PlaneWithinPolygon))
         {
             foreach (var hit in hits)
             {
                 ARPlane plane = planeManager.GetPlane(hit.trackableId);
                 if (plane != null && plane.alignment == PlaneAlignment.Vertical)
                 {
-                    if (paintedWalls.ContainsKey(plane))
-                    {
-                        // Обновляем цвет существующей стены
-                        var existingWall = paintedWalls[plane];
-                        var renderer = existingWall.GetComponent<MeshRenderer>();
-                        renderer.material.color = colorManager.GetCurrentColor();
-                        Debug.Log($"WallPaintingManager: Обновлен цвет существующей стены: {colorManager.GetCurrentColor()}");
-                    }
-                    else
-                    {
-                        // Создаем новый объект для покраски
-                        GameObject coloredWall = new GameObject($"PaintedWall_{plane.trackableId}");
-                        
-                        // Располагаем чуть впереди и делаем немного больше
-                        coloredWall.transform.position = plane.transform.position + plane.transform.forward * 0.0005f;
-                        coloredWall.transform.rotation = plane.transform.rotation;
-                        coloredWall.transform.localScale = new Vector3(1.02f, 1.02f, 1.0f);
-
-                        MeshFilter meshFilter = coloredWall.AddComponent<MeshFilter>();
-                        MeshRenderer meshRenderer = coloredWall.AddComponent<MeshRenderer>();
-
-                        var planeMeshFilter = plane.GetComponent<MeshFilter>();
-                        if (planeMeshFilter != null && planeMeshFilter.mesh != null)
-                        {
-                            meshFilter.mesh = planeMeshFilter.mesh;
-                            Material newMaterial = new Material(wallMaterial);
-                            newMaterial.color = colorManager.GetCurrentColor();
-                            
-                            meshRenderer.material = newMaterial;
-                            meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
-                            meshRenderer.receiveShadows = false;
-
-                            paintedWalls.Add(plane, coloredWall);
-                            Debug.Log($"WallPaintingManager: Создана новая покрашенная стена: {colorManager.GetCurrentColor()}");
-                        }
-                    }
-                    return;
+                    return plane;
                 }
             }
         }
-        else
+        return null;
+    }
+
+    private void UndoLastAction()
+    {
+        if (undoStack.Count == 0) return;
+
+        var action = undoStack.Pop();
+        if (paintedWalls.TryGetValue(action.planeId, out var wallInfo))
         {
-            Debug.Log("WallPaintingManager: Raycast не попал ни в одну плоскость");
+            wallInfo.targetColor = action.previousColor;
         }
     }
 
     void OnDisable()
     {
-        foreach (var wall in paintedWalls.Values)
+        if (previewObject != null)
         {
-            if (wall != null)
+            Destroy(previewObject);
+        }
+
+        foreach (var wallInfo in paintedWalls.Values)
+        {
+            if (wallInfo.gameObject != null)
             {
-                Destroy(wall);
+                Destroy(wallInfo.gameObject);
             }
         }
         paintedWalls.Clear();
+        undoStack.Clear();
+    }
+
+    private class PaintedWallInfo
+    {
+        public GameObject gameObject;
+        public Color currentColor;
+        public Color targetColor;
+
+        public PaintedWallInfo(GameObject obj, Color color)
+        {
+            gameObject = obj;
+            currentColor = color;
+            targetColor = color;
+        }
+    }
+
+    private struct PaintAction
+    {
+        public TrackableId planeId;
+        public Color previousColor;
+        public Color newColor;
+
+        public PaintAction(TrackableId id, Color prev, Color next)
+        {
+            planeId = id;
+            previousColor = prev;
+            newColor = next;
+        }
     }
 }
